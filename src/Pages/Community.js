00f14e06b8e5dc164b6971f16deb26f4b36b6fe9
Mouriem <mourienpacheco@gmail.com>
2023-05-05 00:06:16 -0300

    start toplist

--- src/Pages/Community.js
+++ src/Pages/Community.js
@@ -1,7 +1,11 @@
 import { BannerCommunity } from "../components/BannerCommunity/BannerCommunity";
+import { Toplist } from "../components/Toplist/Toplist";
 
 export function Community(){
     return(
-        <BannerCommunity></BannerCommunity>
+        <div>
+            <BannerCommunity></BannerCommunity>
+            <Toplist></Toplist>
+        </div>
     )
 }
---
+++ src/components/Toplist/Toplist.js
@@ -0,0 +1,20 @@
+import './Toplist.css';
+
+export function Toplist(){
+    return(
+        <div className="toplist">
+            <div className="description">
+                <div className="titleToplist">LBQ Connect</div>
+                <div className="text">LBQ Connect é um programa feminista global que
+                visa impulsionar o trabalho e a visibilidade do ativismo lésbico,
+                bissexual e queer em todo o mundo.</div>
+
+            </div>
+            <div className="picture"></div>
+            <div className="topics">
+                <div className="titleTopics">Destaques do programa</div>
+                <div className="list"></div>
+            </div>
+        </div>
+    )
+}
