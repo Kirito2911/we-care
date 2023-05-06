395dfd619415bf5279c783a117434d799aa3dc1e
Mouriem <mourienpacheco@gmail.com>
2023-05-06 00:12:36 -0300

    create Republic

---
+++ src/components/Republic/Republic.js
@@ -0,0 +1,41 @@
+import './Republic.css';
+
+export function Republic(){
+    return(
+        <div className="republic">
+            <div className="repTitle">Repúblicas</div>
+            <div className="find">
+                <div className="theme">
+                    <div className="category">Tema</div>
+                    <div className="label">Todos os Tópicos</div>
+                </div>
+                <div className="theme">
+                    <div className="category">País/Região</div>
+                    <div className="label">Todas as regiões/países</div>
+                </div>
+                <div className="button">Procurar</div>
+            </div>
+            <div className="news">
+                <div className="new">
+                    <div className="picRep">
+                        <div className="desc">DIREITOS HUMANOS</div>
+                    </div>
+                    <div className="titleNews">Relatório de Impacto do Fundo</div>
+                </div>
+                <div className="new">
+                    <div className="picRep">
+                        <div className="desc">DISCRIMINAÇÃO LEGAL</div>
+                    </div>
+                    <div className="titleNews">Uma montanha em meus ombros:</div>
+                </div>
+                <div className="new">
+                    <div className="picRep">
+                        <div className="desc">SAÚDE MENTAL</div>
+                    </div>
+                    <div className="titleNews">LGBTQ vive em conflito e crise</div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
